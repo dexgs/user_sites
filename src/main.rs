56862cd5e1c7cce43bd296d3f2c1374a39da51d9
chili-b <dexter.gaonshatford@gmail.com>
2021-07-01 15:17:01 -0400

    Implement handling POST requests

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use anyhow::Error;
 use std::thread;
 use std::path::{Path, PathBuf, Component};
 use std::fs::{OpenOptions, File};
-use std::io::Result;
+use std::io::{Result, Read, Write};
 use std::result::Result as StdResult;
 use std::sync::{Arc, Mutex};
 use std::process::{Command, Stdio};
@@ -21,6 +21,7 @@ type SharedData = Arc<Mutex<HashMap<PathBuf, (usize, Option<String>)>>>;
 // The max number of clients which are allowed to view a single page at once
 const MAX_CONCURRENT_ACCESSORS: usize = 5000;
 
+
 fn main() -> StdResult<(), Error> {
     let port = env::args().nth(1).unwrap().parse()?;
     let server = MicroHTTP::new(("0.0.0.0", port))?;
@@ -35,6 +36,7 @@ fn main() -> StdResult<(), Error> {
         }
     }
 }
+
 
 fn handle_client(mut client: Client, shared: SharedData) -> Option<()> {
     let (path, request) = client.request_mut().take()?;
@@ -67,12 +69,13 @@ fn handle_client(mut client: Client, shared: SharedData) -> Option<()> {
     update_shared_data(&shared, &file_path, UpdateType::Accessing);
     if let Err(e) = match request {
         Request::GET(query, _) => handle_get(&file_path, query, client, &shared),
-        Request::POST(_, data) => handle_post(&file_path, &data, client)
+        Request::POST(_, mut data) => handle_post(&file_path, &mut data, client)
     } { eprintln!("{}", e); }
     update_shared_data(&shared, &file_path, UpdateType::Closing);
 
     Some(())
 }
+
 
 // Return file handle and reported file size in bytes
 fn open_file(file_path: PathBuf) -> Result<(File, usize)> {
@@ -80,6 +83,7 @@ fn open_file(file_path: PathBuf) -> Result<(File, usize)> {
     let file_size = file_handle.metadata()?.len() as usize;
     Ok((file_handle, file_size))
 }
+
 
 // Helper function to respond to GET requests
 fn handle_get(file_path: &PathBuf, mut query: HashMap<String, String>, mut client: Client, shared: &SharedData) -> Result<()> {
@@ -140,14 +144,64 @@ fn handle_get(file_path: &PathBuf, mut query: HashMap<String, String>, mut clien
     Ok(())
 }
 
-fn handle_post(file_path: &PathBuf, data: &Option<FormData>, mut client: Client) -> Result<()> {
+
+// Helper function to respond to POST requests
+fn handle_post(file_path: &PathBuf, data: &mut Option<FormData>, mut client: Client) -> Result<()> {
+    let mut file_path = file_path.to_owned();
+
+    // Unlike GET requests, POST requests MUST be handled by an executable
+    if !file_path.ends_with("form_executable") {
+        file_path.push("form_executable");
+    }
+    // If the executable path does not exist (or the points to a directory), exit.
+    if !file_path.exists() || !file_path.is_file() {
+        client.respond("404 Not Found", error_pages::ERROR_404.as_bytes(), &vec![])?;
+        return Ok(());
+    }
+    let executable_path = file_path.as_os_str();
+    let mut command = Command::new(executable_path);
+    command.arg(file_path)
+        .stdout(Stdio::piped())
+        .stdin(Stdio::null());
+    // Different data will be fed to the executable depending on how the form
+    // was encoded.
+    match data.as_mut() {
+        // URL encoded form
+        Some(FormData::KeyVal(vars)) => {
+            filter_env_variables(vars);
+            command.envs(vars);
+        },
+        // Plaintext form
+        Some(FormData::Text(text)) => {
+            command.arg(text);
+        },
+        // Multipart form
+        Some(FormData::Stream(_)) => {
+            command.stdin(Stdio::piped());
+        },
+        _ => {}
+    }
+    let mut child_process = command.spawn()?;
+    if let Some(FormData::Stream(mut reader)) = data.take() {
+        thread::spawn(move || {
+            let mut buffer = [0; 4096];
+            while let Ok(bytes_read) = reader.read(&mut buffer) {
+                if bytes_read == 0 { break; }
+                stdin.write_all(&buffer)
+                    .expect("Writing to stdin");
+            }
+        });
+    }
+    client.respond_ok_chunked(child_process.stdout.expect("Capturing stdout"), usize::MAX)?;
     Ok(())
 }
+
 
 enum UpdateType {
     Accessing,
     Closing
 }
+
 
 fn update_shared_data(shared: &SharedData, path: &PathBuf, update_type: UpdateType) {
     let mut lock = shared.lock().unwrap();
@@ -181,15 +235,18 @@ fn update_shared_data(shared: &SharedData, path: &PathBuf, update_type: UpdateTy
     }
 }
 
+
 fn set_cache(shared: &SharedData, path: &PathBuf, new_cache: &String) {
     if let Some((_, cache)) = shared.lock().unwrap().get_mut(path) {
         *cache = Some(new_cache.to_owned());
     }
 }
 
+
 fn get_cache(shared: &SharedData, path: &PathBuf) -> Option<String> {
     Some(shared.lock().unwrap().get(path)?.1.as_ref()?.to_owned())
 }
+
 
 fn get_concurrent_accessors(shared: &SharedData, path: &PathBuf) -> usize {
     if let Some((accessors, _)) = shared.lock().unwrap().get(path) {
@@ -198,6 +255,7 @@ fn get_concurrent_accessors(shared: &SharedData, path: &PathBuf) -> usize {
         0
     }
 }
+
 
 // Filter out variable definitions that are already present
 fn filter_env_variables(vars: &mut HashMap<String, String>) {
