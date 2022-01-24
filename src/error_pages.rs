0bf48de4988f7fece2197ca0f9c66d81e4ffd5a9
chili-b <chili-b@noreply.localhost>
2022-01-23 23:19:40 -0500

    Update 'src/error_pages.rs'
    Fix the title tag on error pages

--- src/error_pages.rs
+++ src/error_pages.rs
@@ -1,8 +1,8 @@
-pub const ERROR_404: &'static str = format_html!("Nothing",
+pub const ERROR_404: &'static str = format_html!("<title>Nothing</title>",
     "<h1>The page you are looking for does not exist.</h1>");
 
-pub const ERROR_500: &'static str = format_html!("Error",
+pub const ERROR_500: &'static str = format_html!("<title>Error</title>",
     "<h1>The file you requested exists, but could not be served to you due to some error.</h1>");
 
-pub const ERROR_503: &'static str = format_html!("Server Busy",
+pub const ERROR_503: &'static str = format_html!("<title>Server Busy</title>",
     "<h1>Server too busy to serve response. Sorry.</h1>");
